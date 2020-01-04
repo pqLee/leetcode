1ad48b3ebc35c36b9405bbaf19de34c39237ceda
pengqiang <pengqiang.li@intel.com>
2020-01-04 14:53:07 +0800

    heap gif

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,6 +1,84 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
+void heapify(std::vector<int>& arr, int i, int len)
+{
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int largest = i;   // 记录父节点、左节点、右节点中最大值位置
+    if (left < len && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+    
+    if (right < len && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    // 如果最大值不是父节点
+    if (largest != i)  
+    {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // 一直向下递归调用，直到该节点满足：父节点大于等于左右两个子节点
+        heapify(arr, largest, len);
+    }
+}
+
+// 建堆时从底向上调整
+void buildHeap(std::vector<int>& arr, int len)
+{
+    for (int i = len/2; i >= 0; i--)
+    {
+        heapify(arr, i, len);
+    }
+}
+
+
+
+void heapSort(std::vector<int>& arr)
+{
+    int len = arr.size();
+
+    buildHeap(arr, len);
+
+    for (int i = len - 1; i > 0; i--)
+    {
+        int temp = arr[i];
+        arr[i] = arr[0];
+        arr[0] = temp;
+        len--;
+        heapify(arr, 0, len);
+    }
+}
+
+
+int main()
+{
+    // std::vector<int> nums = {5, 1, 13, 3, 16, 7, 10, 14, 6, 9};
+    std::vector<int> nums = {91, 83, 87, 79, 72, 43, 38, 38, 9, 87};
+    
+    // initHeap(nums, nums.size());
+    // buildHeap(nums, nums.size());
+
+    heapSort(nums);
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::cout << nums[i] << " " ;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+
+
+/*
 void initHeap(std::vector<int>& arr, int size)
 {
     // 寻找最后一个节点的父节点
@@ -37,19 +115,5 @@ void initHeap(std::vector<int>& arr, int size)
         }
     }
 }
-
-int main()
-{
-    // std::vector<int> nums = {5, 1, 13, 3, 16, 7, 10, 14, 6, 9};
-    std::vector<int> nums = {91, 83, 87, 79, 72, 43, 38};
-    initHeap(nums, nums.size());
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        std::cout << nums[i] << " " ;
-    }
-        std::cout << std::endl;
-
-    return 0;
-}
+*/
 
